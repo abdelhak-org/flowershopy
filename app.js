c9a834ebcd30dd8857d7a1ed552e29466e4a51ee
abdelhak-org <abdelhak.org@gmail.com>
2022-09-06 20:19:15 +0200

    add javascript

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const scrollBtn = document.querySelector(".scroll-btn");
+const links = document.querySelectorAll(".container .nav ul li");
+
+
+
+
+
+
 scrollBtn.addEventListener("click",()=>{
     window.scrollTo(0,{behavior:"smooth"})
 }
-)
+);
+
+links.forEach(link =>{
+    link.addEventListener("click", (e)=>{
+        e.preventDefault;
+        const id = e.target.getAttribute("data-link");
+        console.log(id);
+        document.getElementById(id).scrollIntoView({behavior:"smooth"})
+
+    })
+})
